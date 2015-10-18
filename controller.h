ba26041fb3f433faa9ea17f813c25dd6acee2ec8
chriswald <wald.j.chris@gmail.com>
2015-10-18 10:55:33 -0500

    Provide common controller functionality.

--- controller.h
+++ controller.h
@@ -1,5 +1,17 @@
 #include <stdint.h>
 
+#define CTRL_A      0
+#define CTRL_B      1
+#define CTRL_SELECT 2
+#define CTRL_START  3
+#define CTRL_UP     4
+#define CTRL_DOWN   5
+#define CTRL_LEFT   6
+#define CTRL_RIGHT  7
+
+#define PLAYER_1    0
+#define PLAYER_2    1
+
 typedef struct
 {
     uint8_t LATCH, CLK, DATA;
@@ -7,3 +19,6 @@ typedef struct
 } Controller;
 
 extern Controller CTRL[2];
+
+void initControllers();
+void getControllerState(uint8_t player);

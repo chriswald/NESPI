69f5ce28e8b620bfdfdf18d0c264c25aecf2b34f
Chris Wald <wald.j.chris@gmail.com>
2015-10-18 14:59:03 -0500

    Added keyup and keydown support.

--- keys.c
+++ keys.c
@@ -1,1 +1,19 @@
 #include "keys.h"
+#include <xdo.h>
+
+xdo_t* x;
+
+void initKeys()
+{
+	x = xdo_new(":0.0");
+}
+
+void keyDown(const char* keyseq)
+{
+	xdo_keysequence_down(x, CURRENTWINDOW, keyseq, 0);
+}
+
+void keyUp(const char* keyseq)
+{
+	xdo_keysequence_up(x, CURRENTWINDOW, keyseq, 0);
+}
--- keys.h
+++ keys.h
@@ -1,6 +1,17 @@
 #ifndef __KEYS_H__
 #define __KEYS_H__
 
+#define K_W     "W"
+#define K_A     "A"
+#define K_S     "S"
+#define K_D     "D"
+#define K_Up    "Up"
+#define K_Left  "Left"
+#define K_Down  "Down"
+#define K_Right "Right"
 
+void initKeys();
+void keyDown(const char* keyseq);
+void keyUp(const char* keyseq);
 
 #endif // __KEYS_H__
